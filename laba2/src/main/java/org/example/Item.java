66680e2e5b02f74a88412d75eca323e2387c5ce9
Bohdan <kbs20043131@gmail.com>
2024-01-08 20:59:37 +0200

    laba2
    Add all files

---
+++ laba2/src/main/java/org/example/Item.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Item {
+    private final String title;
+    private String identifier;
+
+    public Item(String title, String identifier) {
+        this.title = title;
+        this.identifier = identifier;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getType() {
+        return "Предмет";
+    }
+
+    @Override
+    public String toString() {
+        return getType() + ": " + title + " (Ідентифікатор: " + identifier + ")";
+    }
+}
